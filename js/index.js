24ce4c88b14a0748539edcbe4d0fa0c4ac50c940
Toufiq Shishir <toufiq_a24@outlook.com>
2023-07-17 14:41:30 +0600

    -

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-const navLinks = document.querySelector('.nav-links')
+const navLinks = document.querySelector(".nav-links");
 
-function onToggleMenu(e){
-    e.name = e.name === 'menu' ? 'close' : 'menu'
-    navLinks.classList.toggle('top-[9%]')
+function onToggleMenu(e) {
+  e.name = e.name === "menu" ? "close" : "menu";
+  navLinks.classList.toggle("top-[9%]");
 }
